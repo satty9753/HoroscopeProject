4654954ea4b90a0fde6cf1013b1253289fa12cbb
michelle <michelle@marcde-macbook-pro-2.local>
2018-12-17 10:17:09 +0800

    Initial Commit

---
+++ HoroscopeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HoroscopeProject
+//
+//  Created by michelle on 2018/12/17.
+//  Copyright © 2018年 michelle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
